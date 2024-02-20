3c1201c9f8d99acd8546355a533ae51dea43ed55
anir7702 <112372931+anir7702@users.noreply.github.com>
2024-02-20 16:43:32 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,47 +1,21 @@
-pipeline {
+pipeline{
     agent any
- 
-    stages {
-        stage('Checkout') {
-            steps {
-                // Checkout the source code from the Git repository
-                git credentialsId: 'anir7702', url: 'https://github.com/anir7702/python-web-app'
+    
+    stages{
+        stage('checkout'){
+            steps{
+                git branch: 'main', url: 'https://github.com/anir7702/python-web-app.git'
             }
         }
- 
-        stage('Install dependencies') {
-            steps {
-                // Install Python dependencies using pip
+        stage('Install Dependencies'){
+            steps{
                 sh 'pip install -r requirements.txt'
             }
         }
- 
-        stage('Run tests') {
-            steps {
-                // Run tests using pytest or any other testing framework
-                sh 'pytest'
+        stage('Test'){
+            steps{
+                sh 'export PATH="$PATH:/usr/local/bin/pytest"'
             }
-        }
- 
-        stage('Build') {
-            steps {
-                // Build the Flask application (if applicable)
-                // This step could involve running any build commands or scripts
-            }
-        }
- 
-        stage('Deploy') {
-            steps {
-                // Deploy the Flask application (e.g., copy files to a server)
-                // This step could involve running any deployment commands or scripts
-            }
-        }
-    }
- 
-    post {
-        always {
-            // Clean up or perform any post-build actions
         }
     }
 }
- 
